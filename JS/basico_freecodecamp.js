2969ea97a1531942821e373c3855c44e09c601af
MarcosPBackes <mpb.inv@gmail.com>
2023-02-01 21:56:28 -0300

    primeiro commit

---
+++ JS/basico_freecodecamp.js
@@ -0,0 +1,44 @@
+/*-----------------CONTAR CARTAS----------------
+Você vai escrever uma função de contagem de cartas.
+A função receberá um parâmetro card, que pode ser 
+um número ou uma string, e incrementar ou decrementar
+a variável global count de acordo com o valor da carta.
+Em seguida, a função retornará a string com o valor atual
+de contagem (variável count) e a string Bet se a contagem 
+for positiva, ou Hold se a contagem for zero ou negativa. 
+A contagem atual e a decisão do jogador (Bet ou Hold) 
+deve ser separado por um único espaço.*/
+let count = 0;
+function cc(card) {        
+    switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        count++;
+        break;
+      case 10:
+      case 'J':
+      case 'Q':
+      case 'K':
+      case 'A':    
+        count--;
+        break;
+    }
+    if (count > 0) {
+      return count + ' Bet';
+    }  
+    else {
+      return count + ' Hold';
+    }    
+  }
+  
+  cc(2); cc(3); cc(4); cc(5); cc(6);
+  cc(7); cc(8); cc(9);
+  cc(10); cc('J'); cc('Q'); cc('K'); cc('A');
+  cc(3); cc(7); cc('Q'); cc(8); cc("a");
+  cc(2); cc('J'); cc(9); cc(2); cc(7);
+  cc(2); cc(2); cc(10);
+  cc(3); cc(2); cc('A'); cc(10); cc('K');
+  /*---------------------------------------------------------------------------*/
