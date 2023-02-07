216fb1604386330b257ae87e64afa6617a698d1b
MarcosPBackes <mpb.inv@gmail.com>
2023-02-07 20:52:48 -0300

    Manipulaçao de promessas

--- JS/es6_freecodecamp.js
+++ JS/es6_freecodecamp.js
@@ -347,3 +347,100 @@ thermos.temperature = 26;
 temp = thermos.temperature; //26 em Celsius
 
 /*---------------------------------------------------------------------------*/
+
+/*-------------------------------CRIAR UM PROMESA-----------------------------
+Uma promessa em JavaScript é exatamente o que parece - você faz a promessa 
+de que vai fazer uma tarefa, geralmente de forma assíncrona. Quando a tarefa 
+é finalizada, ou você cumpriu a promessa ou falhou ao tentar. Por ser uma função 
+construtora, você precisa utilizar a palavra-chave new para criar uma Promise. 
+Ela recebe uma função, como seu argumento, com dois parâmetros - resolve e reject. 
+Esses métodos são usados para determinar o resultado da promessa.*/
+
+//exemplos
+
+const myPromise = new Promise((resolve, reject) => {
+
+});
+
+const makeServer = new Promise((resolve, reject) => {
+
+});
+
+/*---------------------------------------------------------------------------*/
+
+/*-----------CONCLUIR UMA PROMESA COM RESOLVE E REJECT-----------------------
+Uma promessa possui três estados: pendente (pending), cumprida (fulfilled) e 
+rejeitada (rejected). A promessa que você criou no desafio anterior está presa 
+no estado pending para sempre porque você não adicionou uma forma de concluir a 
+promessa. Os parâmetros resolve e reject passados para o argumento da promessa 
+servem para este propósito. resolve é utilizado quando a promessa for bem-sucedida, 
+enquanto reject é utilizado quando ela falhar. Ambos são métodos que recebem apenas
+ um argumento*/
+
+//exemplo
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer;
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {	
+    reject("Data not received");
+  }
+});
+
+/*---------------------------------------------------------------------------*/
+
+/*-----------------MANIPULAR UMA PROMESSA CUMPRIDA USANDO THEN----------------
+Promessas são úteis quando você tem um processo que leva uma quantidade de 
+tempo desconhecido para ser finalizado (ou seja, algo assíncrono). Muitas vezes, 
+uma requisição a um servidor. Fazer uma requisição a um servidor leva tempo, 
+e após a requisição ser finalizada, você geralmente quer fazer algo com a 
+esposta retornada. Isso pode ser feito usando o método then. O método then é 
+executado imediatamente após a promessa ser cumprida com resolve*/
+
+//exemplo
+
+const makeRequest = new Promise((resolve, reject) => {
+  
+  let responseFromServer = true;
+	
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {	
+    reject("Data not received");
+  }
+});
+
+makeRequest.then(result => {
+  console.log(result);
+});
+
+/*---------------------------------------------------------------------------*/
+
+/*------------------MANIPULAR UMA PROMESSA REJEITADA USANDO CATCH--------------
+catch é o método usado quando a promessa é rejeitada. Ele é executado imediatamente 
+após o método reject da promessa ser chamado.*/
+
+//exemplos
+
+const serverRequest = new Promise((resolve, reject) => {
+  
+  let responseFromServer = false;
+	
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {	
+    reject("Data not received");
+  }
+});
+
+serverRequest.then(result => {
+  console.log(result);
+});
+
+serverRequest.catch(error => {
+  console.log(error);
+});
+
+/*---------------------------------------------------------------------------*/
+
