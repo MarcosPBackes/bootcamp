0cae3f3782c9a52cbd85b5ed3caeab58d57bfb33
MarcosPBackes <mpb.inv@gmail.com>
2023-02-08 09:14:18 -0300

    Metodo .test com multiplas possibilidades

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -21,3 +21,21 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString);
 
 /*---------------------------------------------------------------------------*/
+
+/*---------------METODO TEST COM DIFERENTES POSSIBLIDADES--------------------
+Ao usar regexes como /coding/, você pode procurar pelo padrão coding em strings.
+Isso funciona com strings únicas, mas é limitado a apenas um padrão. Você pode 
+procurar por múltiplos padrões usando o operador de alternation, ou OR: |.
+Este operador funciona para buscar padrões à esquerda e à direita dele.
+Por exemplo, se você quiser encontrar as strings yes ou no, a regex que 
+você quer é /yes|no/. Você pode também procurar por mais de dois padrões 
+com este operador. É possível fazer isso ao adicionar mais instâncias do operador 
+seguido do padrão desejado: /yes|no|maybe/.*/
+
+//exemplo
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Altere esta linha
+let resposta = petRegex.test(petString);
+
+/*---------------------------------------------------------------------------*/
