d6768d555894351f1321dcef4e67303f6396296b
MarcosPBackes <mpb.inv@gmail.com>
2023-02-08 11:30:35 -0300

    Encontrar com caractere curinga

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -84,3 +84,11 @@ let starRegex = /twinkle/gi; //multiplas flags g = multipla busca i = ignorecase
 let startResult = twinkleStar.match(starRegex);
 
 /*---------------------------------------------------------------------------*/
+
+/*-------------------ENCONTAR COM O CARACTERE CURINGA--------------------------
+Haverá vezes em que você não saberá (ou não precisará saber) quais caracteres 
+exatamente farão parte das suas regexes. Pensar em todas as palavras que 
+capturariam, digamos, um erro ortográfico levaria muito tempo. Por sorte, 
+você pode economizar tempo usando o caractere curinga: . O caractere curinga . 
+captura qualquer caractere. O curinga também pode ser chamado de ponto. 
+Você pode usar o curinga como qualquer outro caractere na regex. Por exemplo, e você quiser encontrar hug, huh, hut ou hum, você pode usar a regex /hu./ para capturar todas as quatro palavras.
