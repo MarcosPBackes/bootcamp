1164691bacec8e713f206d07ff5a98bc8e679d92
MarcosPBackes <mpb.inv@gmail.com>
2023-02-09 15:11:39 -0300

    Especificar apenas um minimo de capturas

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -394,3 +394,26 @@ let ohResult = ohRegex.test(ohStr);
 
 /*---------------------------------------------------------------------------*/
 
+/*-----------------ESPECIFICAR APENAS UM MINIMO DE CAPTURAS--------------------
+Você pode especificar um número mínimo e um máximo de capturas com chaves. 
+Haverá vezes que você precisará especificar um mínimo mas não um máximo.
+Para fazer isso, apenas escreva o número seguido de uma vírgula dentro das chaves.
+
+Por exemplo, para capturar pelo menos 3 vezes a letra a na string hah você pode escrever a 
+regex /ha{3,}h/.*/
+
+let A5 = "haaaah";
+let A3 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleA5 = /ha{3,}h/;
+multipleA5.test(A5);//Retorna true
+multipleA5.test(A3);//Retorna false
+multipleA5.test(A100);//Retorna true
+
+//Modifique a regex haRegex para que capture quatro ou mais z's na string Hazzah.
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/;
+let haResult = haRegex.test(haStr);
+
+/*---------------------------------------------------------------------------*/
