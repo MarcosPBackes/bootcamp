81a46e15700043973dd9e1d419d195587ff39a8e
MarcosPBackes <mpb.inv@gmail.com>
2023-02-09 15:35:38 -0300

    Verificar existencia

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -439,3 +439,25 @@ let timRegex = /Tim{4}ber/;//Captura 4 m's na string
 let timResult = timRegex.test(timStr);
 
 /*---------------------------------------------------------------------------*/
+
+/*--------------------------VERIFICAR EXISTENCIA------------------------------
+Haverá vezes em que você procurará padrões que podem ou não existir na string.
+Pode ser relevante validá-los dependendo da situação. Você pode fazer com que 
+um padrão seja opcional ao usar uma interrogação, ?, depois dele. Ela valida 
+se há uma ou nenhuma ocorrência do padrão. Pode-se dizer que a interrogação 
+torna o elemento à esquerda dela opcional.
+
+Por exemplo, com a interrogação você pode capturar palavras em inglês 
+escritas com a ortografia americana ou britânica.*/
+
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american);//Retorna true
+rainbowRegex.test(british);//Retorna true
+
+let favWord = "favorite";
+let favRegex = /favou?rite/;
+let favResult = favRegex.test(favWord);
+
+/*---------------------------------------------------------------------------*/
