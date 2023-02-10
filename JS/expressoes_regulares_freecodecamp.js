b9857a694e465e5d264b835d676d40389bb158bd
MarcosPBackes <mpb.inv@gmail.com>
2023-02-10 09:24:16 -0300

    Reusar padroes com grupos de caaptura

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -519,3 +519,35 @@ let rooseveltRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/;
 let rooseveltResult = myRegex.test(rooseveltString);
 
 /*---------------------------------------------------------------------------*/
+
+/*----------------REUSAR PADROES COM GRUPOS DE CAPTURA------------------------
+Vamos supor que você deseja encontrar a correspondência de uma palavra que 
+ocorra várias vezes como abaixo.*/
+
+let repeatStr = "row row row your boat";
+
+/*Você poderia usar /row row row/, mas e se você não souber a palavra específica 
+repetida? Grupos de captura podem ser usados para localizar substrings repetidas.
+Os grupos de captura são criados envolvendo o padrão de regex a ser capturado 
+entre parênteses. Neste caso, o objetivo é capturar uma palavra composta de 
+caracteres alfanuméricos para que o grupo de captura seja \w+ entre parênteses: 
+/(\w+)/. A substring correspondente ao grupo é salva em uma "variável" temporária 
+que pode ser acessada dentro da mesma expressão regular usando uma barra invertida 
+e o número do grupo de captura (ex.: \1). Os grupos de captura são automaticamente 
+numerados pela posição de abertura de seus parênteses (esquerda para direita), 
+começando em 1. O exemplo abaixo captura qualquer palavra que se repita três vezes,
+separada por espaços:*/
+
+let repeatRegex = /(\w+) \1 \1/;
+repeatRegex.test(repeatStr); //Retorna true
+repeatStr.match(repeatRegex); //Retorna ["row row row", "row"]
+
+/*Use grupos de captura na regex reRegex para capturar em uma 
+string um número que aparece exatamente três vezes, separados <==
+por espaços.*/                                                      
+
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/;//<==
+let reResult = reRegex.test(repeatNum);
+
+/*---------------------------------------------------------------------------*/
