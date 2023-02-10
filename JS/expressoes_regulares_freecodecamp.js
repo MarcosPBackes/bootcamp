c0ce8056eebbeb0cf8884a24819106f2f864fb7d
MarcosPBackes <mpb.inv@gmail.com>
2023-02-10 08:16:39 -0300

    Usar lookheads positivos e negativos

--- JS/expressoes_regulares_freecodecamp.js
+++ JS/expressoes_regulares_freecodecamp.js
@@ -461,3 +461,40 @@ let favRegex = /favou?rite/;
 let favResult = favRegex.test(favWord);
 
 /*---------------------------------------------------------------------------*/
+
+/*-------------------USAR LOOKHEADS POSITIVOS E NEGATIVOS---------------------
+Lookaheads ("olhar à frente") são padrões que dizem ao JavaScript para procurar 
+outros padrões ao longo da string sem capturá-los. Eles podem ser úteis quando 
+é necessário fazer diversas verificações na mesma string.
+Existem dois tipos de lookahead: o lookahead positivo e o lookahead negativo.
+Lookaheads positivos garantem que o padrão especificado se encontra à frente, 
+mas não o capturam. Usa-se (?=...), onde ... é o padrão a ser procurado, 
+para escrever lookaheads positivos.
+Lookaheads negativos, por outro lado, garantem que o padrão especificado não 
+se encontra à sua frente na string. Para usar lookaheads negativos, escrevemos 
+(?!...) onde ... é o padrão que você quer ter certeza que não está lá. O restante 
+do padrão é validado se o padrão do lookahead negativo estiver ausente.*/
+
+//exemplos
+
+let quit = "qu";
+let noquit = "qt";
+let quRegex= /q(?=u)/;
+let qRegex = /q(?!u)/;
+quit.match(quRegex);//retorna ['q']
+noquit.match(qRegex);//retorna ['q']
+
+/*Validar dois padrões diferentes em uma string é considerado 
+um uso mais prático de lookaheads. Neste não tão aprimorado validador 
+de senhas, os lookaheads procuram por 3 a 6 caracteres e pelo menos 
+um número, respectivamente, na string:*/
+
+let password = "abc123";
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+checkPass.test(password);
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/;
+let pwResult = pwRegex.test(sampleWord);
+
+/*---------------------------------------------------------------------------*/
