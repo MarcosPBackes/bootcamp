6ce277ff288f1e50f6fe645d2a6e0fd41776bda5
MarcosPBackes <mpb.inv@gmail.com>
2023-02-10 10:24:17 -0300

    Depuraçao: Usar o console.log para verificar dados

---
+++ JS/depuracao_freecodecamp.js
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------*/
+
+/*--------------USAR O CONSOLE PARA VERIFICAR VALORES------------------------
+O método console.log(), o qual "imprime" a saída do que está nos seus parênteses 
+no console, provavelmente será a ferramenta de debug mais útil. Colocá-lo em 
+pontos estratégicos no seu código pode mostrar a você os valores intermediários 
+de variáveis. É uma boa prática ter uma ideia do que deveria ser a saída antes 
+de olhar o que é. Ter pontos de verificação para ver o status de seus cálculos 
+ao longo do seu código ajudará a encontrar onde o problema está.*/
+
+//exemplo
+
+console.log('Hello world!');//Imprime Hello world!
+
+let a = 5;
+let b = 1;
+a++;
+console.log(a)//Imprime 6
+let sumAB = a + b;
+console.log(sumAB);//Imprime 7
+
+/*---------------------------------------------------------------------------*/
