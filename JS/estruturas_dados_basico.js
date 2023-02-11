2251180934fc62bdbedca11ed221a9794547dfed
MarcosPBackes <mpb.inv@gmail.com>
2023-02-11 09:31:31 -0300

    Estruturas de dadods basicos: Usar array para armazenar uma coloeçao de dados

---
+++ JS/estruturas_dados_basico.js
@@ -0,0 +1,40 @@
+/*---------------------------------------------------------------------------*/
+
+/*------------------USAR ARRAY PARA ARMAZENAR UM COLEÇAO DE DADOS--------------
+Abaixo está um exemplo da implementação mais simples de uma estrutura de dados 
+array. Isso é conhecido como array unidimensional, significando que tem apenas 
+1 nível de profundidade, ou que não possui nenhum outro array aninhado dentro 
+de si. Note que possui booleans, strings e numbers, entre outros tipos de dados 
+do JavaScript válidos:*/
+
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+
+//Isso é conhecido como um array multidimensional, ou um array que 
+//contém outros arrays. Note que esse array também contém 
+//objetos JavaScript,
+
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+  ];
+
+/*---------------------------------------------------------------------------*/
+  
