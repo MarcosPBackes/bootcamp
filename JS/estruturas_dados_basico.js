3881955a5ceda075cd8e0a23a2beeb0682d3e2fd
MarcosPBackes <mpb.inv@gmail.com>
2023-02-12 11:01:06 -0300

    Copiar usando spread

--- JS/estruturas_dados_basico.js
+++ JS/estruturas_dados_basico.js
@@ -180,3 +180,31 @@ function forecast(arr) {
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
 /*---------------------------------------------------------------------------*/
+
+/*-------------------COPIAR UM ARRAY COM O OPERADOR SPREAD---------------------
+Enquanto slice() nos permite sermos seletivos sobre quais elementos de um array 
+copiar, entre várias outras tarefas úteis, o novo operador spread do ES6 nos 
+permite facilmente copiar todos os elementos de um array, em ordem, com uma 
+sintaxe simples e altamente legível. A sintaxe de spread é simplesmente essa: ...*/
+
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+
+/*thatArray é igual a [true, true, undefined, false, null]. 
+thisArray permanece inalterado e thatArray contém os mesmos 
+elementos que thisArray.*/
+
+//exemplo
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {    
+    newArr.push([...arr])    
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+/*---------------------------------------------------------------------------*/
