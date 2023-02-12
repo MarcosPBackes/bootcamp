ee77eb865ca6050625f27accb8485544a9597e99
MarcosPBackes <mpb.inv@gmail.com>
2023-02-12 10:48:33 -0300

    Copiar usando slice()

--- JS/estruturas_dados_basico.js
+++ JS/estruturas_dados_basico.js
@@ -155,3 +155,28 @@ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush',
     'PaleTurquoise', 'FireBrick']));
 
 /*---------------------------------------------------------------------------*/
+
+/*----------------COPIAR ITENS DE UM ARRAY USANDO SLICE()---------------------
+O próximo método que abordaremos é slice(). Em vez de modificar um array, 
+slice() copia ou extrai um determinado número de elementos para um novo 
+array, deixando o array em que o método é chamado inalterado. slice() recebe 
+apenas 2 parâmetros — o primeiro é o índice aonde começar a extração e o 
+segundo é o índice no qual parar a extração (a extração ocorrerá até esse 
+índice mas não o incluirá). Considere isto:*/
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3);
+
+/*todaysWeather teria o valor ['snow', 'sleet'],
+enquanto weatherConditions ainda teria ['rain', 'snow', 'sleet', 'hail', 'clear'].*/
+
+//exemplo
+
+function forecast(arr) {  
+  arr = arr.slice(2, 4);
+  return arr;//Ou return arr.slice(2, 4);
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+/*---------------------------------------------------------------------------*/
