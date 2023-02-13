83246157c4fe4c60add2891a26a0ca78b9e99fe0
MarcosPBackes <mpb.inv@gmail.com>
2023-02-13 09:40:14 -0300

    Verificar a presença de um elemento com idexOf()

--- JS/estruturas_dados_basico.js
+++ JS/estruturas_dados_basico.js
@@ -230,3 +230,31 @@ function spreadOut() {
 console.log(spreadOut());
 
 /*---------------------------------------------------------------------------*/
+
+/*-------------VERIFICAR A PRESENÇA DE UM ELEMENTO COM INDEXOS()--------------
+Já que arrays podem ser alterados, ou mutados, a qualquer momento, não há 
+garantia de onde um dado estará em um determinado array, ou se esse elemento 
+sequer existe. Felizmente, o JavaScript nos fornece outro método integrado, 
+indexOf(), que nos permite rapidamente e facilmente checar pela presença de 
+um elemento em um array. indexOf() recebe um elemento como parâmetro, e quando 
+chamado, retorna a posição, ou índice, daquele elemento, ou -1 se o elemento 
+não existe no array.*/
+
+//exemplos
+
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+fruits.indexOf('dates');//Retorna -1
+fruits.indexOf('oranges');//Retorna 2
+fruits.indexOf('pears');//Retorna 1
+
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+/*---------------------------------------------------------------------------*/
