2bae482f668a17641435c86ad2ff4a3c50651fdf
MarcosPBackes <mpb.inv@gmail.com>
2023-02-13 14:03:27 -0300

    Adicionar pares de chave-valor a objetos

--- JS/estruturas_dados_basico.js
+++ JS/estruturas_dados_basico.js
@@ -296,7 +296,7 @@ console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 
 /*---------------------------------------------------------------------------*/
 
-/*------------------CRIAR ARRAYS MULTIDDIMENSIONAIS COMPLEXOS-----------------
+/*------------------CRIAR ARRAYS MULTIDIMENSIONAIS COMPLEXOS-----------------
 Excelente! Você acabou de aprender muito sobre arrays! Esta foi uma visão geral
 de nível bastante elevado, e há muito mais a aprender para trabalhar com arrays. 
 Muitas dessas questões você verá em sessões posteriores. Mas antes de passarmos 
@@ -338,5 +338,68 @@ let myNestedArray = [
   ['mutate', 1327.98, 'splice', 'slice', 'push'],
   ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
 ];
+
+/*---------------------------------------------------------------------------*/
+
+/*-------------ADICIONAR PARES DE CHAVE-VALOR A OBJETOS------------------------
+Em suas formas mais básicas, objetos são apenas coleções de pares de chave-valor.
+Em outras palavras, eles são pedaços de dados (valores) mapeados para 
+identificadores únicos chamados propriedades (chaves).*/
+
+const tekkenCharacter = {
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true
+};
+
+/*O código acima define um objeto de personagens do jogo de videogame 
+Tekken chamado tekkenCharacter. Tem três propriedades, em que cada uma 
+é mapeada para um valor específico. Se você quer adicionar uma propriedade 
+adicional, como "origin", isso pode ser feito atribuindo origin ao objeto:*/
+
+tekkenCharacter.origin = 'South Korea';
+
+/*Isso usa a notação de ponto. Se você observar o objeto tekkenCharacter,
+ele agora incluirá a propriedade origin. Hwoarang também tinha cabelos 
+cor de laranja, bem diferentes. Você pode adicionar essa propriedade 
+com notação de colchetes fazendo:*/
+
+tekkenCharacter['hair color'] = 'dyed orange';
+
+/*A notação de colchete é necessária se sua propriedade tem um 
+espaço nela ou se você deseja usar uma variável para nomear a 
+propriedade. No caso acima, a propriedade está entre aspas para 
+denotá-la como uma string e será adicionada exatamente como 
+mostrada. Sem aspas, ela será avaliada como uma variável e o 
+nome da propriedade será qualquer valor que a variável for.*/
+
+const eyes = 'eye color';
+
+tekkenCharacter[eyes] = 'brown';
+
+//Após adicionar todos os exemplos, o objeto ficará assim:
+
+/*{
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true,
+  origin: 'South Korea',
+  'hair color': 'dyed orange',
+  'eye color': 'brown'
+};*/
+
+//exemplo
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+
+console.log(foods);
 
 /*---------------------------------------------------------------------------*/
