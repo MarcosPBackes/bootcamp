7890c1f1a99c4f6e2c6f7469b93214e791554d93
MarcosPBackes <mpb.inv@gmail.com>
2023-02-15 22:59:31 -0300

    verificar o construtor de um objeto

--- POO_freecodecamp.js
+++ POO_freecodecamp.js
@@ -152,11 +152,40 @@ function Passaro(name, color) {
     this.numLegs = 2;
 }
 
+let azul = new Passaro('azul', 'azul')
+
 function Cao(name, color,) {
     this.name = name;
     this.color = color;
     this.numLegs = 4
-  }
+}
+
 let terrier = new Cao('Xuxu', 'cinza')  
 
+/*---------------------------------------------------------------------------*/
+
+/*--------------VERIFICAR O CONSTRUTOR DE UM OBJETO COM INSTANCEOF-------------
+Toda vez que a função construtora cria um novo objeto, o objeto é definido como 
+uma instance do seu construtor. JavaScript provê uma forma conveniente para 
+verificar isso com o operador instanceof. instanceof permite que você compare 
+um objeto a um construtor, retornando true ou false caso seja ou não um objeto 
+criado pelo construtor, respectivamente.*/
+
+let corvo = new Passaro('Alexis', 'black');
+corvo instanceof Passaro;//Retorna true
+
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(3)
+myHouse instanceof House;//Retorna true
+
+let canary = {
+    name: "Mildred",
+    color: "Yellow",
+    numLegs: 2
+};
+  
+canary instanceof Passaro;//Retorna falso
+  
 /*---------------------------------------------------------------------------*/
