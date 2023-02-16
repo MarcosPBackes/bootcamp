ad498b2ce0c38e96e819cf920ea42630e8e282a9
MarcosPBackes <mpb.inv@gmail.com>
2023-02-15 22:04:46 -0300

    Criar objeto basico

---
+++ POO_freecodecamp.js
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------*/
+
+/*----------------------------CRIAR UM OBJETO BASICO---------------------------
+Pense sobre as coisas que as pessoas veem todos os dias, como carros, lojas e 
+pássaros. Tudo isso são objetos: coisas tangíveis com que pessoas podem observar 
+e interagir. Quais são algumas qualidades destes objetos? Um carro possui rodas. 
+Lojas vendem itens. Pássaros possuem asas. Estas qualidades, ou propriedades, 
+definem o que faz um objeto. Note que objetos similares compartilham de 
+propriedades iguais, mas podem ter diferentes valores para estas propriedades. 
+Por exemplo, todos os carros possuem rodas, mas nem todos os carros possuem o 
+mesmo número de rodas. Objetos em JavaScript são utilizados como modelos de 
+objetos do mundo real, dando a eles propriedades e comportamentos assim como 
+se fossem análogos ao mundo real.*/
+
+//exemplos
+
+let duck = {
+    name: "Aflac",
+    numLegs: 2
+  };
+
+let dog = {
+name: "Bolt",
+numLegs: 4,
+};
+
+/*---------------------------------------------------------------------------*/
+
