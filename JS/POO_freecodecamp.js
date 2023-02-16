57840c07116c6376fed60f6028aaad0640976d25
MarcosPBackes <mpb.inv@gmail.com>
2023-02-16 15:30:55 -0300

    Sobrescrever metodos herdados

--- JS/POO_freecodecamp.js
+++ JS/POO_freecodecamp.js
@@ -570,3 +570,42 @@ Dog.prototype.bark = function() {
 let marrom = new Dog();
 
 /*---------------------------------------------------------------------------*/
+
+/*---------------------------SOBRESCREVER METODOS HERDADOS--------------------
+Nas lições passadas, você aprendeu que um objeto pode herdar seus comportamentos 
+(métodos) de outro objeto ao referenciar o prototype do objeto:
+É possível sobrescrever um método herdado. É feito da mesma maneira - ao adicionar 
+o método a ChildObject.prototype utilizando o mesmo nome do método que aquele a 
+ser sobrescrito. Aqui está um exemplo de Bird sobrescrevendo o método eat() 
+herdado de Animal:*/
+
+function Animal() { }
+Animal.prototype.eat = function() {
+  return "nom nom nom";
+};
+function Bird() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.eat = function() {
+  return "peck peck peck";
+};
+
+//exemplo
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function() {
+  return "Alas, this is a flightless bird."
+};
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+/*---------------------------------------------------------------------------*/
