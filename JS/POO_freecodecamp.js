78781f6d140f555726a0fca6e61ef41b1a298115
MarcosPBackes <mpb.inv@gmail.com>
2023-02-16 20:45:13 -0300

    Usar closure para evitar que propriedades deum objeto sejam modificadas externamente

--- JS/POO_freecodecamp.js
+++ JS/POO_freecodecamp.js
@@ -658,3 +658,40 @@ glideMixin(bird);
 glideMixin(boat);
 
 /*---------------------------------------------------------------------------*/
+
+/*--USAR CLOSURE PARA EVITAR QUE PROPRIEDADES DE UM OBJETO SEJAM MODIFICADAS EXTERNAMENTE--
+Portanto, qualquer parte do seu código pode facilmente alterar o nome do bird 
+para qualquer valor. Pense sobre coisas como senhas e contas de banco sendo 
+facilmente modificáveis em qualquer parte do seu código. Isso poderia causar 
+inúmeros problemas. A forma mais simples para tornar essa propriedade 
+pública em privada, seria criando uma variável dentro da função constructor.
+Isso alteraria o escopo daquela variável para ser apenas o escopo da função 
+construtora ao invés de globalmente disponível. Dessa maneira, a variável 
+pode ser acessada e modificada apenas pelos métodos dentro da função construtora.*/
+
+function Bird() {
+  let hatchedEgg = 10;
+
+  this.getHatchedEggCount = function() { 
+    return hatchedEgg;
+  };
+}
+let ducky = new Bird();
+ducky.getHatchedEggCount();
+
+/*Aqui getHatchedEggCount é um método privilegiado, porque ele possui acesso à 
+variável privada hatchedEgg. Isso é possível porque hatchedEgg é declarado no
+mesmo contexto que getHatchedEggCount. Em JavaScript, a função sempre possui 
+acesso ao contexto na qual foi criada. Isso é chamado de closure.*/
+
+//exemplos
+
+function Bird() {
+  let weight = 15;
+  this.getWeight = function() {
+    return weight;
+  }
+}
+
+/*---------------------------------------------------------------------------*/
+
