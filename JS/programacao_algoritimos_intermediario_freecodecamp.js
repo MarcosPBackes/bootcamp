ed21719412a728c51cb973b5a89ba759c1348a7a
MarcosPBackes <mpb.inv@gmail.com>
2023-03-02 11:41:12 -0300

    Despachar numeros

--- JS/programacao_algoritimos_intermediario_freecodecamp.js
+++ JS/programacao_algoritimos_intermediario_freecodecamp.js
@@ -462,3 +462,24 @@ function smallestCommons(arr) {
 }
 
 /*---------------------------------------------------------------------------*/
+
+/*--------------------------DESPACHAR NUMEROS---------------------------------
+Dado o array arr, faça iterações sobre ele e remova cada elemento começando do 
+primeiro elemento (o índice 0) até que a função func retorne true quando o 
+elemento iterado for passado através dele.
+Em seguida, retorne o resto do array uma vez que a condição for satisfeita, 
+caso contrário, arr deve ser retornado como um array vazio.*/
+
+function dropElements(arr, func) {
+  // Loop através do array
+  for (let i = 0; i < arr.length; i++) {
+    // Se a função retornar true para o elemento atual, retorna o restante do array
+    if (func(arr[i])) {
+      return arr.slice(i);
+    }
+  }
+  // Se a função nunca retornar true, retorna um array vazio
+  return [];
+}
+
+/*---------------------------------------------------------------------------*/
