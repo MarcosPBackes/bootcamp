ead58cf12d5337303f924a26bebd6e80e06819a1
MarcosPBackes <mpb.inv@gmail.com>
2023-03-03 10:13:08 -0300

    Criar registradora

--- JS/Projetos_de_algoritmos_e_estruturas_de_dados.js
+++ JS/Projetos_de_algoritmos_e_estruturas_de_dados.js
@@ -139,3 +139,92 @@ function telephoneCheck(str) {
 }
 
 /*---------------------------------------------------------------------------*/
+
+/*----------------------------------CRIAR REGISTRADORA----------------------
+Implemente uma função de caixa registradora checkCashRegister() que aceita o 
+preço de compra como o primeiro argumento (price), pagamento como o segundo 
+argumento (cash) e dinheiro na registradora (cid) como o terceiro argumento.
+cid é um array de duas dimensões listando a moeda disponível e a quantidade.
+A função checkCashRegister() deve sempre retornar um objeto com a chave status 
+e uma chave change.
+Retorne {status: "INSUFFICIENT_FUNDS", change: []} se o dinheiro no caixa for 
+menor que o troco devido, ou se você não pode retornar o troco exato.
+Retorne {status: "CLOSED", change: [...]} com dinheiro no caixa como o 
+valor para a chave change se for igual ao troco devido.
+Caso contrário, retorne {status: "OPEN", change: [...]}, com o troco devido 
+em moedas e notas, ordenado do maior para o menor, como o valor da chave change.
+Unidade de moeda	Quantidade
+Penny	$0,01 (PENNY)
+Nickel	$0,05 (NICKEL)
+Dime	$0,1 (DIME)
+Quarter	$0,25 (QUARTER)
+Dólar	$1 (ONE)
+Cinco dólares	$5 (FIVE)
+Dez dólares	$10 (TEN)
+Vinte dólares	$20 (TWENTY)
+Cem dólares	$100 (ONE HUNDRED)
+Veja abaixo um exemplo de um array de dinheiro no caixa (cid):
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]*/
+
+function checkCashRegister(price, cash, cid) {
+    const UNIT_AMOUNTS = {
+      "PENNY": 0.01,
+      "NICKEL": 0.05,
+      "DIME": 0.1,
+      "QUARTER": 0.25,
+      "ONE": 1,
+      "FIVE": 5,
+      "TEN": 10,
+      "TWENTY": 20,
+      "ONE HUNDRED": 100
+    };
+  
+    const totalCid = cid.reduce((acc, curr) => acc + curr[1], 0);
+  
+    let changeDue = cash - price;
+    let change = [];
+  
+    if (changeDue > totalCid) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else if (changeDue.toFixed(2) === totalCid.toFixed(2)) {
+      return {status: "CLOSED", change: cid};
+    } else {
+      cid = cid.reverse();
+  
+      for (let i = 0; i < cid.length; i++) {
+        const unit = cid[i][0];
+        const unitAmount = UNIT_AMOUNTS[unit];
+        let unitTotal = cid[i][1];
+        let unitsToReturn = 0;
+  
+        while (changeDue >= unitAmount && unitTotal > 0) {
+          changeDue -= unitAmount;
+          changeDue = Math.round(changeDue * 100) / 100;
+          unitTotal -= unitAmount;
+          unitsToReturn++;
+        }
+  
+        if (unitsToReturn > 0) {
+          change.push([unit, unitsToReturn * unitAmount]);
+        }
+      }
+    }
+  
+    if (changeDue > 0) {
+      return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+  
+    return {status: "OPEN", change: change};
+  }
+  
+/*---------------------------------------------------------------------------*/
