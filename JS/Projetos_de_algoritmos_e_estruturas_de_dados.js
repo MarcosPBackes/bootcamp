fdfb4a09b3defee0e9e82fbf26a3775b8885f687
MarcosPBackes <mpb.inv@gmail.com>
2023-03-03 09:34:41 -0300

    Conversor de numerais romanos

--- JS/Projetos_de_algoritmos_e_estruturas_de_dados.js
+++ JS/Projetos_de_algoritmos_e_estruturas_de_dados.js
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------*/
 
-/*----------------------------VERIFICADOR DE PALINDRO-------------------------
+/*----------------------------VERIFICADOR DE PALINDROMO-------------------------
 Retorne true se a string fornecida for um palíndromo. Caso contrário, retorne false.
 Um palíndromo é uma palavra ou frase que é dita da mesma maneira na ordem natural 
 que na ordem inversa, ignorando pontuação, capitalização e espaçamento.
@@ -20,6 +20,55 @@ function palindrome(str) {
   
     // Compara a string original com a string invertida
     return str === reversedStr;
-  }
+}
 
-  /*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+
+/*--------------------------CONVERSOR DE NUMERAIS ROMANOS---------------------
+Converta o número dado em um número romano.
+Números romanos 	Números arábicos
+M 	1000
+CM 	900
+D 	500
+CD 	400
+C 	100
+XC 	90
+L 	50
+XL 	40
+X 	10
+IX 	9
+V 	5
+IV 	4
+I 	1
+Todos os números romanos devem ser em maiúsculas.*/
+
+function convertToRoman(num) {
+    const romanNums = [
+      { value: 1000, symbol: "M" },
+      { value: 900, symbol: "CM" },
+      { value: 500, symbol: "D" },
+      { value: 400, symbol: "CD" },
+      { value: 100, symbol: "C" },
+      { value: 90, symbol: "XC" },
+      { value: 50, symbol: "L" },
+      { value: 40, symbol: "XL" },
+      { value: 10, symbol: "X" },
+      { value: 9, symbol: "IX" },
+      { value: 5, symbol: "V" },
+      { value: 4, symbol: "IV" },
+      { value: 1, symbol: "I" },
+    ];
+  
+    let result = "";
+  
+    for (let i = 0; i < romanNums.length; i++) {
+      while (num >= romanNums[i].value) {
+        result += romanNums[i].symbol;
+        num -= romanNums[i].value;
+      }
+    }
+  
+    return result;
+}
+
+/*---------------------------------------------------------------------------*/
